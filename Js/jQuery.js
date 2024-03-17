dc97bcffd6e340c76dcf0de2f5222f5d6bbdfece
Hood <playlistdh@gmail.com>
2024-03-17 14:10:56 +0900

    .

---
+++ Js/jQuery.js
@@ -0,0 +1,77 @@
+// JavaScript source code
+
+$(function () {
+    $(".a-js").on("mouseover", function () {
+        $(".a-js").animate({
+            opacity: "1"
+        },
+            800)
+    })
+});
+
+
+$(function () {
+    $(".d-js").on("mouseover", function () {
+        $(".d-js").animate({
+            opacity: "1"
+        },
+            800)
+    })
+});
+
+
+$(function () {
+    $(".g-js").on("mouseover", function () {
+        $(".g-js").animate({
+            opacity: "1"
+        },
+            800)
+    })
+});
+
+
+$(function () {
+    $(".i-js").on("mouseover", function () {
+        $(".i-js").animate({
+            opacity: "1"
+        },
+            800)
+    })
+});
+
+$(function () {
+    $(".aw-js").on("mouseover", function () {
+        $(".award").animate({
+            width: "90%"
+        },
+            2000)
+        })
+});
+
+$(function () {
+    $(".aw-js").on("mouseover", function () {
+        $(".in-class").animate({
+            width: "65%"
+        },
+            2000)
+    })
+});
+
+$(function () {
+    $(".aw-js").on("mouseover", function () {
+        $(".out-class").animate({
+            width: "50%"
+        },
+            2000)
+    })
+});
+
+$(function () {
+    $(".aw-js").on("mouseover", function () {
+        $(".participation").animate({
+            width: "70%"
+        },
+            2000)
+    })
+});
+
